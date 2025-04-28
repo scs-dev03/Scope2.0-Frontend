55045cac6fb6234c16ce7b6fee07f454db21d965
spareXcode <ayush.s@sparecare.in>
2025-04-28 15:36:14 +0530

    first commit and change in angular cli version and primeng

--- src/app/von/von-routing.module.ts
+++ src/app/von/von-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminRemarkComponent } from './admin-remark/admin-remark.component';
 import { AdminpendingcountComponent } from './adminpendingcount/adminpendingcount.component';
+import { DealerVonComponent } from './dealer-von/dealer-von.component';
+import { AdminVonComponent } from './admin-von/admin-von.component';
 
 const routes: Routes = [
 
@@ -11,7 +13,13 @@ const routes: Routes = [
   },{
     path: 'pcount',
     component: AdminpendingcountComponent
-  },
+  },{
+    path: 'dvon',
+    component: DealerVonComponent
+  },{
+    path: 'avon',
+    component: AdminVonComponent
+  }
 ];
 
 @NgModule({
