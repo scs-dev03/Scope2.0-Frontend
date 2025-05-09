9111809f0110c3b8b8c37707c2f6a363de89dcce
spareXcode <ayush.s@sparecare.in>
2025-05-09 20:35:52 +0530

    change to live web10

--- src/app/services/Von/dealervonservice.service.ts
+++ src/app/services/Von/dealervonservice.service.ts
@@ -5,17 +5,17 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class DealervonserviceService {
-//   private apiurl: any =
-//   'http://web10.185.238.new.ocpwebserver.com/api/v1/von/';
-
-// private apiurlmaster: any =
-//   'http://web10.185.238.new.ocpwebserver.com/api/v1/master/';
-
-   private apiurl: any =
-  'https://6mztnd0t-3000.inc1.devtunnels.ms/api/v1/von/';
+  private apiurl: any =
+  'http://web10.185.238.new.ocpwebserver.com/api/v1/von/';
 
 private apiurlmaster: any =
-  'https://6mztnd0t-3000.inc1.devtunnels.ms/api/v1/master/';
+  'http://web10.185.238.new.ocpwebserver.com/api/v1/master/';
+
+//    private apiurl: any =
+//   'https://6mztnd0t-3000.inc1.devtunnels.ms/api/v1/von/';
+
+// private apiurlmaster: any =
+//   'https://6mztnd0t-3000.inc1.devtunnels.ms/api/v1/master/';
 
 dealerId: any = 8
 brandID: any = 9
