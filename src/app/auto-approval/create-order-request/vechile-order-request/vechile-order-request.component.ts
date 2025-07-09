0f07078ce3d161db69e89fbbccd754f8f819c35f
sparexcode <ayush.s@sparecare.in>
2025-07-09 18:00:58 +0530

    functionalit of vo is completed but need to add nishant sir changes

--- src/app/auto-approval/create-order-request/vechile-order-request/vechile-order-request.component.ts
+++ src/app/auto-approval/create-order-request/vechile-order-request/vechile-order-request.component.ts
@@ -3,7 +3,7 @@ import { SHARED_IMPORTS } from '../../../shared/shared-imports/shared-module';
 import { PrimengModuleModule } from '../../../shared/primeng-module/primeng-module.module';
 import { SharedModule } from 'primeng/api';
 import { DividerModule } from 'primeng/divider';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import * as XLSX from 'xlsx';
 import * as FileSaver from 'file-saver';
 
@@ -15,49 +15,83 @@ import * as FileSaver from 'file-saver';
   styleUrl: './vechile-order-request.component.css'
 })
 export class VechileOrderRequestComponent {
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.addPart()
+  }
 
   AddPartWise: FormGroup
+  VechileOrderRequestInput: FormGroup
 
   constructor(private fb: FormBuilder) {
-    this.AddPartWise = this.fb.group({
-      PartNumber: (''),
-      Quantity: (''),
-      Remark: ('')
+     this.AddPartWise = this.fb.group({
+      parts: this.fb.array([]) // FormArray to hold each part form group
+    });
+
+    this.VechileOrderRequestInput = this.fb.group({
+      VechileNumber: ['',[Validators.required,Validators.pattern(/^[A-Za-z0-9]{10}$/)]],
+      VechileModel: ['',Validators.required],
+      JobCardNumber: ['',[Validators.required,Validators.pattern(/^[A-Za-z0-9]{15}$/)]],
+      JobCardType: ['',Validators.required],
+      Advisor: ['',Validators.required],
+      OrderType: ['',Validators.required],
     })
   }
 
+    get parts() {
+    return this.AddPartWise.get('parts') as FormArray;
+  }
 
+   addPart() {
+    const part = this.fb.group({
+      PartNumber: ['', [Validators.required]],
+      Quantity: ['', [Validators.required]],
+      Remark: ['', [Validators.required]],
+    });
 
-  SampleExcelDownload() {
+    this.parts.push(part);
+  }
+
+   removePart(index: number) {
+    this.parts.removeAt(index);
+  }
   
-      const Data = [
-        {
-          'Vehicle_Number': '',
-          'Vehicle_Model': '',
-          'Job_Card_Number': '',
-          'Advisor': '',
-          'JOb_Card_Type': '',
-          'Order_Type': ''
+    SampleExcelDownload() {
+    
+        const Data = [
+          {
+            'Vehicle_Number': '',
+            'Vehicle_Model': '',
+            'Job_Card_Number': '',
+            'Advisor': '',
+            'JOb_Card_Type': '',
+            'Order_Type': ''
+          }
+        ];
+    
+        const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(Data)
+    
+        const workbook: XLSX.WorkBook = {
+          Sheets: { 'SampleData': worksheet },
+          SheetNames: ['SampleData']
         }
-      ];
-  
-      const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(Data)
-  
-      const workbook: XLSX.WorkBook = {
-        Sheets: { 'SampleData': worksheet },
-        SheetNames: ['SampleData']
+    
+        const ExcelBuffer: any = XLSX.write(workbook, {
+          type: 'array',
+          bookType: 'xlsx'
+        })
+    
+        const data: Blob = new Blob([ExcelBuffer], {
+          type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    
+        });
+    
+        FileSaver.saveAs(data, 'Sample_Download.xlsx');
       }
-  
-      const ExcelBuffer: any = XLSX.write(workbook, {
-        type: 'array',
-        bookType: 'xlsx'
-      })
-  
-      const data: Blob = new Blob([ExcelBuffer], {
-        type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-  
-      });
-  
-      FileSaver.saveAs(data, 'Sample_Download.xlsx');
-    }
+
+
 }
+
+
+
