d793ce168a76cbe57e26cf471c6aef2ae1713c2b
sparexcode <ayush.s@sparecare.in>
2025-07-23 18:47:23 +0530

    update in dashboard service

--- src/app/services/dashboard-scheduler/dashboard-scheduler.service.ts
+++ src/app/services/dashboard-scheduler/dashboard-scheduler.service.ts
@@ -35,7 +35,7 @@ constructor(private http: HttpClient) {}
 // }
 getDashboard(data: any): Observable<any> {
   return this.http.post(
-    `${this.apiurlmaster}dashboardscheduler/getdashboard`,
+    `${this.apiurlmaster}getdashboard`,
     data
   );
 }
